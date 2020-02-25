4b7c4d2a4624f20ff6d37f61d3bf2ace83e95602
Hirofumi Nakajo <hirofuminakajo@Hirofumi-Nakajos-MBP.local>
2020-02-26 02:18:49 +0900

    finish test

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
+    int i = 0;
+
     cout << "test" << endl;
     cout << "hoge" << endl;
+    for (i = 0; i < 7; i++)
+    {
+        i *= 2;
+        cout << i << endl;
+    }
     return 0;
 }
